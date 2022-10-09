0976adef10643b3b9ba4cad80828f8641a17ea3d
S W <sw@Ss-MacBook-Air.local>
2022-10-09 14:00:19 -0500

    Interfaces completed

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,19 +1,32 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+
+        flavors.sort(comparator);
+        cones.sort(new ConeComparator());
+
+
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
 
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
 
+
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println(flavors);
+        System.out.println(cones);
     }
+
 }
